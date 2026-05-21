28a41be6f74d67135c3ebc6a2b31c26b6f244baf
rex2630 <rex2630@gmail.com>
2026-05-21 09:48:36 +0200

    Update NovatekHiHzProtocol.kt

--- app/src/main/java/com/kooduXA/opendash/data/protocol/NovatekHiHzProtocol.kt
+++ app/src/main/java/com/kooduXA/opendash/data/protocol/NovatekHiHzProtocol.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
